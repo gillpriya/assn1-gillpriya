b339acfc0f33afe33f420eb502e13db2c84c6fe2
gillpriya <43182866+gillpriya@users.noreply.github.com>
2018-09-26 11:59:12 -0700

    Add files via upload

--- assn1-gillpriya-master/src/main/java/ca/uvic/seng330/assn1/PlayerComparator.java
+++ assn1-gillpriya-master/src/main/java/ca/uvic/seng330/assn1/PlayerComparator.java
@@ -3,7 +3,8 @@ package ca.uvic.seng330.assn1;
 import java.util.Comparator;
 
 public class PlayerComparator implements Comparator<Player> {
-
+	
+@Override
 	public int compare(Player p1, Player p2) {
 		if (p1.getPoints() > p2.getPoints()) {
 			return 1;
